3e79288f46c63da9f09a5b2a7bd7b61427f3e89f
izanSV <100832315+izanSV@users.noreply.github.com>
2024-05-19 01:08:35 +0200

    Update Setting.cs

--- Servicios/Implementacion/Setting.cs
+++ Servicios/Implementacion/Setting.cs
@@ -3,12 +3,12 @@
     public static class Setting
     {
 
-        internal const string FireBaseDatabaseUrl = "https://alkila-bbdd-default-rtdb.firebaseio.com";
-        internal const string FireBaseSeceret = "hlMeApXA8mURYnAl9pIBuBP9Ago6msE5PxfX2uyw";
-        internal const string FirebaseApiKey = "AIzaSyDV4KGSEvHf8VOqnLEbO93CjkgesPxlgGg";
-        internal const string FirebaseStoreBucket = "alkila-bbdd.appspot.com";
-        internal const string FotoPredeterminada = "https://firebasestorage.googleapis.com/v0/b/alkila-bbdd.appspot.com/o/FotoPerfil.jpeg?alt=media&token=72d2a9e6-63e9-4b00-8040-24f3370dc765";
-        internal const string ApiKeyWhatsApp = "EABnu11GthvsBOzkfyQ2dhoNhpD3LfQW04xsjxvQ1NXq3ZBCFZBtPXu3jglpzZBJmbnZCnk69hzEQUpt5GA4FfykxqbV1ebCjujTZBmGHvPa18ZAesf7kRK3rdfC3oTLlarw6PufmBBjveanfhfiHUwyhaAf4s8mn0IwZAmsNVNuOpbPJSZCGXTOPln0y1xlK0G5bI5gfoJfG5HVCnkGSR2xr1WZBAxO4UXzPzjEokNZBgZD";
+        internal const string FireBaseDatabaseUrl = "***";
+        internal const string FireBaseSeceret = "***";
+        internal const string FirebaseApiKey = "***";
+        internal const string FirebaseStoreBucket = "***";
+        internal const string FotoPredeterminada = "https://firebasestoragepeg?alt=media&token=72d2a9e6-63e9-4b00-8040-24f3370dc765";
+        internal const string ApiKeyWhatsApp = "y1xlK0G5bI5gfoJfG5HVCnkGSR2xr1WZBAxO4UXzPzjEokNZBgZD";
         internal const string FotoWhatsApp = "https://blog.logrocket.com/wp-content/uploads/2023/02/dotnet-maui-guide-complete-overview-demo.png";
     }
 }
